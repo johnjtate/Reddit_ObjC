5a1075aaf1a87e053935e3dd143306d3f8121c5b
John Tate <johntate@Johns-MacBook-Pro.local>
2018-09-12 10:15:00 -0600

    Initial commit

---
+++ Reddit_ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reddit_ObjC
+//
+//  Created by John Tate on 9/12/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
